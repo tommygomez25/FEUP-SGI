96e239ea37873e95044f04a8c938dad9151f4803
tommygomez25 <maysan54321@gmail.com>
2023-10-04 19:46:12 +0100

    Finished textures and lighting

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -21,12 +21,40 @@ class MyContents  {
         this.lastBoxEnabled = null
         this.boxDisplacement = new THREE.Vector3(0,2,0)
 
+        this.wallMeshes = []
+
         // plane related attributes
         this.diffusePlaneColor = "#00ffff"
         this.specularPlaneColor = "#777777"
         this.planeShininess = 30
         this.planeMaterial = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
             specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess, side: THREE.DoubleSide })
+
+        this.tampoTexture = new THREE.TextureLoader().load( 'textures/wood.jpg' );
+        this.tampoTexture.wrapS = THREE.RepeatWrapping;
+        this.tampoTexture.wrapT = THREE.RepeatWrapping;
+        this.tampoPlaneMaterial = new THREE.MeshPhongMaterial({ map: this.tampoTexture, specular: "#dcdcdc", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+        
+        this.tomasTexture = new THREE.TextureLoader().load( 'textures/tomas.jpg' );
+        this.tomasTexture.wrapS = THREE.RepeatWrapping;
+        this.tomasTexture.wrapT = THREE.RepeatWrapping;
+        this.tomasPlaneMaterial = new THREE.MeshPhongMaterial({ map: this.tomasTexture, specular: "#dcdcdc", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+
+        this.franciscoTexture = new THREE.TextureLoader().load( 'textures/francisco.jpg' );
+        this.franciscoTexture.wrapS = THREE.RepeatWrapping;
+        this.franciscoTexture.wrapT = THREE.RepeatWrapping;
+        this.franciscoPlaneMaterial = new THREE.MeshPhongMaterial({ map: this.franciscoTexture, specular: "#dcdcdc", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+        
+        this.viewTexture = new THREE.TextureLoader().load( 'textures/view.jpg' );
+        this.viewTexture.wrapS = THREE.RepeatWrapping;
+        this.viewTexture.wrapT = THREE.RepeatWrapping;
+        this.viewPlaneMaterial = new THREE.MeshPhongMaterial({ map: this.viewTexture, specular: "#dcdcdc", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+        
+        this.cakeTexture = new THREE.TextureLoader().load( 'textures/cake.jpg' );
+        this.cakeTexture.wrapS = THREE.RepeatWrapping;
+        this.cakeTexture.wrapT = THREE.RepeatWrapping;
+        this.cakePlaneMaterial = new THREE.MeshPhongMaterial({ map: this.cakeTexture, specular: "#000000", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+
     }
 
     /**
@@ -69,6 +97,8 @@ class MyContents  {
         const ambientLight = new THREE.AmbientLight( 0x555555 );
         this.app.scene.add( ambientLight );
 
+        this.addSpotlights()
+        
         this.buildBox()
         
         this.createWalls()
@@ -78,6 +108,73 @@ class MyContents  {
         this.createPlate()
 
         this.createCake()
+
+        this.addBoards()
+
+    }
+
+    addSpotlights() {
+        this.spotLight = new THREE.SpotLight( 0xffffff, 15 );
+        this.spotLight.position.set(0,5,0);
+        this.spotLight.angle = Math.PI/20;
+        this.spotLight.penumbra = 0
+        this.spotLight.decay = 0
+        this.spotLight.distance = 10
+        this.spotLight.intensity = 1.2
+
+        this.spotLightTarget = new THREE.Object3D()
+        this.spotLightTarget.position.set(0,1,0)
+        this.app.scene.add(this.spotLightTarget)
+
+        this.spotLight.target = this.spotLightTarget
+        this.app.scene.add(this.spotLight)
+
+        this.spotLight2 = new THREE.SpotLight( 0xffffff, 15 );
+        this.spotLight2.position.set(-2.5,5,2.5);
+        this.spotLight2.angle = Math.PI/10;
+        this.spotLight2.penumbra = 1
+        this.spotLight2.decay = 0
+        this.spotLight2.distance = 10
+        this.spotLight2.intensity = 1.2
+
+        this.spotLightTarget2 = new THREE.Object3D()
+        this.spotLightTarget2.position.set(-4.9,2.8,2.5)
+        this.app.scene.add(this.spotLightTarget2)
+
+        this.spotLight2.target = this.spotLightTarget2
+        this.app.scene.add(this.spotLight2)
+
+        this.spotLight3 = new THREE.SpotLight( 0xffffff, 15 );
+        this.spotLight3.position.set(-2.5,5,-2.5);
+        this.spotLight3.angle = Math.PI/10;
+        this.spotLight3.penumbra = 1
+        this.spotLight3.decay = 0
+        this.spotLight3.distance = 10
+        this.spotLight3.intensity = 1.2
+
+        this.spotLightTarget3 = new THREE.Object3D()
+        this.spotLightTarget3.position.set(-4.9,2.8,-2.5)
+        this.app.scene.add(this.spotLightTarget3)
+
+        this.spotLight3.target = this.spotLightTarget3
+        this.app.scene.add(this.spotLight3)
+
+
+        this.spotLight4 = new THREE.SpotLight( 0xffffff, 15 );
+        this.spotLight4.position.set(0,6,-2);
+        this.spotLight4.angle = Math.PI/12;
+        this.spotLight4.penumbra = 1
+        this.spotLight4.decay = 0
+        this.spotLight4.distance = 10
+        this.spotLight4.intensity = 1.2
+
+        this.spotLightTarget4 = new THREE.Object3D()
+        this.spotLightTarget4.position.set(0,2.8,-5)
+        this.app.scene.add(this.spotLightTarget4)
+
+        this.spotLight4.target = this.spotLightTarget4
+        this.app.scene.add(this.spotLight4)
+        
 
     }
 
@@ -100,10 +197,21 @@ class MyContents  {
     }
 
     createTable() {
+        
+        let planeSizeU = 10;
+        let planeSizeV = 7;
+        let planeUVRate = planeSizeV / planeSizeU;
+        let tampoTextureUVRate = 350 / 268; // image dimensions
+        let tampoTextureRepeatU = 1;
+        let tampoTextureRepeatV =tampoTextureRepeatU * planeUVRate * tampoTextureUVRate;
+        this.tampoTexture.repeat.set(tampoTextureRepeatU, tampoTextureRepeatV );
+        this.tampoTexture.rotation = 0;
+        this.tampoTexture.offset = new THREE.Vector2(0,0);
+
         let tampo = new THREE.BoxGeometry(2.5, 0.1, 2.5);
-        let tampoMaterial = new THREE.MeshPhongMaterial({ color: "#d2b48c",specular: "#000000", emissive: "#000000", shininess: 90 });
-        this.tampoMesh = new THREE.Mesh( tampo, tampoMaterial);
+        this.tampoMesh = new THREE.Mesh( tampo, this.tampoPlaneMaterial);
         this.tampoMesh.position.y = 1;
+
         this.app.scene.add( this.tampoMesh);
 
         this.addLegs(this.tampoMesh);
@@ -132,8 +240,7 @@ class MyContents  {
     
     addLeg(width, height, depth) {
         const legGeometry = new THREE.BoxGeometry(width, height, depth);
-        const legMaterial = new THREE.MeshPhongMaterial({ color: "#d2b48c", specular: "#000000", emissive: "#000000", shininess: 90 });
-        const legMesh = new THREE.Mesh(legGeometry, legMaterial);
+        const legMesh = new THREE.Mesh(legGeometry, this.tampoPlaneMaterial);
         return legMesh;
     }
 
@@ -148,6 +255,8 @@ class MyContents  {
         this.wallMesh.rotation.y = ry;
         this.wallMesh.rotation.z = rz;
         this.app.scene.add( this.wallMesh );
+
+        this.wallMeshes.push(this.wallMesh);
     }
 
     createPlate() {
@@ -160,9 +269,18 @@ class MyContents  {
     }
 
     createCake() {
+        let planeSizeU = 3;
+        let planeSizeV = 2;
+        let planeUVRate = planeSizeV / planeSizeU;
+        let cakeUVRate = 350/233; // image dimensions
+        let cakeRepeatU =4
+        let cakeRepeatV =cakeRepeatU * planeUVRate * cakeUVRate;
+        this.cakeTexture.repeat.set(cakeRepeatU, cakeRepeatV );
+        this.cakeTexture.rotation = 0;
+        this.cakeTexture.offset = new THREE.Vector2(0,0);
+
         const cakeGeometry = new THREE.CylinderGeometry(0.35, 0.35, 0.1, 32, 1, false, 0, 1/(2 * Math.PI/32)); // Raio superior, raio inferior, altura, segmentos, stacks, openEnded, abertura inicial e ângulo thetaLength
-        const cakeMaterial = new THREE.MeshPhongMaterial({ color: "#ffd700", specular: "#ffd700", emissive: "#ffd700", shininess: 90 });
-        this.cake = new THREE.Mesh(cakeGeometry, cakeMaterial);
+        this.cake = new THREE.Mesh( cakeGeometry,this.cakePlaneMaterial)
         this.cake.position.y = this.plate.position.y + this.plate.geometry.parameters.height / 2 + this.cake.geometry.parameters.height / 2;
 
         const candleGeometry = new THREE.CylinderGeometry(0.01, 0.01, 0.1, 32);
@@ -180,6 +298,56 @@ class MyContents  {
         this.app.scene.add(this.cake);
     }
 
+    addBoards() {
+        let planeSizeU = 10;
+        let planeSizeV = 10;
+        let planeUVRate = planeSizeV / planeSizeU;
+        let tomasUVRate = 800 / 800; // image dimensions
+        let tomasRepeatU = 1;
+        let tomasRepeatV =tomasRepeatU * planeUVRate * tomasUVRate;
+        this.tomasTexture.repeat.set(tomasRepeatU, tomasRepeatV );
+        this.tomasTexture.rotation = 0;
+        this.tomasTexture.offset = new THREE.Vector2(0,0);
+
+        let board_tomas = new THREE.PlaneGeometry( 3, 3 );
+        this.board_tomasMesh = new THREE.Mesh( board_tomas, this.tomasPlaneMaterial );
+        this.board_tomasMesh.rotation.y = Math.PI / 2;
+        this.board_tomasMesh.position.y = 2.5;
+        this.board_tomasMesh.position.x = this.wallMeshes[0].position.x + 0.01;
+        this.board_tomasMesh.position.z = 2.5;
+        this.app.scene.add( this.board_tomasMesh );
+
+        let franciscoUVRate = 800 / 800; // image dimensions
+        let franciscoRepeatU = 1;
+        let franciscoRepeatV =franciscoRepeatU * planeUVRate * franciscoUVRate;
+        this.franciscoTexture.repeat.set(franciscoRepeatU, franciscoRepeatV );
+        this.franciscoTexture.rotation = 0;
+        this.franciscoTexture.offset = new THREE.Vector2(0,0);
+
+        let board_francisco = new THREE.PlaneGeometry( 3, 3 );
+        this.board_franciscoMesh = new THREE.Mesh( board_francisco, this.franciscoPlaneMaterial );
+        this.board_franciscoMesh.rotation.y = Math.PI / 2;
+        this.board_franciscoMesh.position.y = 2.5;
+        this.board_franciscoMesh.position.x = this.wallMeshes[0].position.x + 0.01;
+        this.board_franciscoMesh.position.z = -2.5;
+        this.app.scene.add( this.board_franciscoMesh );
+
+        let viewUVRate = 800 / 800; // image dimensions
+        let viewRepeatU = 1;
+        let viewRepeatV =viewRepeatU * planeUVRate * viewUVRate;
+        this.viewTexture.repeat.set(viewRepeatU, viewRepeatV );
+        this.viewTexture.rotation = 0;
+        this.viewTexture.offset = new THREE.Vector2(0,0);
+        
+        let board_view = new THREE.PlaneGeometry( 3, 3 );
+        this.board_viewMesh = new THREE.Mesh( board_view, this.viewPlaneMaterial );
+        this.board_viewMesh.position.y = 2.5;
+        this.board_viewMesh.position.x =0
+        this.board_viewMesh.position.z = this.wallMeshes[2].position.z + 0.01;
+        this.app.scene.add( this.board_viewMesh );
+
+
+    }
 
     
     /**
