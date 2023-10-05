89734bd5533b4b56f9f4c6feae2b7cda7bb5c4bf
tommygomez25 <maysan54321@gmail.com>
2023-10-05 15:25:07 +0100

    Added carocha and spiral

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { MyAxis } from './MyAxis.js';
 import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import { MyNurbsBuilder } from './MyNurbsBuilder.js';
+
 
 
 /**
@@ -26,12 +28,18 @@ class MyContents  {
 
         this.wallMeshes = []
 
-        // plane related attributes
-        this.diffusePlaneColor = "#00ffff"
-        this.specularPlaneColor = "#777777"
-        this.planeShininess = 30
-        this.planeMaterial = new THREE.MeshStandardMaterial({ color: this.diffusePlaneColor, roughness:0.1,
-            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess, side: THREE.DoubleSide })
+        // NURBS
+        this.builder = new MyNurbsBuilder()
+        this.meshes = []
+        this.samplesU = 8         // maximum defined in MyGuiInterface
+        this.samplesV = 8         // maximum defined in MyGuiInterface
+        this.createNurbsSurfaces()
+        
+        // textures
+        this.wallTexture = new THREE.TextureLoader().load( 'textures/wall.jpg' );
+        this.wallTexture.wrapS = THREE.RepeatWrapping;
+        this.wallTexture.wrapT = THREE.RepeatWrapping;
+        this.wallPlaneMaterial = new THREE.MeshStandardMaterial({ map: this.wallTexture, emissive: "#000000", side: THREE.DoubleSide });
 
         this.tampoTexture = new THREE.TextureLoader().load( 'textures/wood.jpg' );
         this.tampoTexture.wrapS = THREE.RepeatWrapping;
@@ -59,7 +67,11 @@ class MyContents  {
         this.cakeTexture.wrapT = THREE.RepeatWrapping;
         this.cakePlaneMaterial = new THREE.MeshPhongMaterial({ map: this.cakeTexture, specular: "#000000", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
 
-    }
+        this.boardTexture = new THREE.TextureLoader().load( 'textures/board.jpg' );
+        this.boardTexture.wrapS = THREE.RepeatWrapping;
+        this.boardTexture.wrapT = THREE.RepeatWrapping;
+        this.boardMaterial = new THREE.MeshPhongMaterial({ map: this.boardTexture, specular: "#000000", emissive: "#000000", shininess: 10, side: THREE.DoubleSide });
+    }   
 
     /**
      * builds the box mesh with material assigned
@@ -101,6 +113,11 @@ class MyContents  {
         const ambientLight = new THREE.AmbientLight( 0x555555 );
         this.app.scene.add( ambientLight );
 
+        //curves
+        this.quadraticBezierCurve = null
+        this.catmullRomCurve = null
+        this.numberOfSamples = 100
+
         this.addSpotlights()
         
         this.buildBox()
@@ -117,6 +134,160 @@ class MyContents  {
 
         this.addBoards()
 
+        this.recompute()
+
+    }
+
+    /**
+
+     * removes (if existing) and recreates the nurbs surfaces
+
+     */
+
+    createNurbsSurfaces() {  
+        // are there any meshes to remove?
+        if (this.meshes !== null) {
+           // traverse mesh array
+            for (let i=0; i<this.meshes.length; i++) {
+                // remove all meshes from the scene
+                this.app.scene.remove(this.meshes[i])
+            }
+            this.meshes = [] // empty the array  
+        }
+
+        // declare local variables
+        let controlPoints;
+        let surfaceData;
+        let mesh;
+        let orderU = 2
+        let orderV = 3
+        // build nurb #1
+        controlPoints =
+
+        [   // U = 0
+
+                [ // V = 0..3;
+
+                    [ -1.5, -1.5, 0.0, 1 ],
+
+                    [ -2.0, -2.0, 2.0, 1 ],
+
+                    [ -2.0,  2.0, 2.0, 1 ],
+
+                    [ -1.5,  1.5, 0.0, 1 ]
+
+                ],
+
+            // U = 1
+
+                [ // V = 0..3
+
+                    [ 0.0,  0.0, 3.0, 1 ],
+
+                    [ 0.0, -2.0, 3.0, 1 ],
+
+                    [ 0.0,  2.0, 3.0, 1 ],
+
+                    [ 0.0,  0.0, 3.0, 1 ]        
+
+                ],
+
+            // U = 2
+
+                [ // V = 0..3
+
+                    [ 1.5, -1.5, 0.0, 1 ],
+
+                    [ 2.0, -2.0, 2.0, 1 ],
+
+                    [ 2.0,  2.0, 2.0, 1 ],
+
+                    [ 1.5,  1.5, 0.0, 1 ]
+
+                ]
+
+         ]
+
+       
+
+        surfaceData = this.builder.build(controlPoints,
+
+                      orderU, orderV, this.samplesU,
+
+                      this.samplesV, this.material)  
+
+        mesh = new THREE.Mesh( surfaceData, this.material );
+        mesh.rotation.x = 0
+        mesh.rotation.y = 0
+        mesh.rotation.z = 0
+        mesh.scale.set( 1,1,1 )
+        mesh.position.set( 0,0,0 )
+        this.app.scene.add( mesh )
+        this.meshes.push (mesh)
+    }
+
+    drawSemiCircunference(position, radius, numberOfSamples) {
+        let points = []
+        const inicialAngle = 0
+        const finalAngle = Math.PI
+
+        for (let i = 0; i <= numberOfSamples; i++) {
+            const t = i / this.numberOfSamples
+            const x = radius * Math.cos(inicialAngle + t * (finalAngle - inicialAngle))
+            const y = radius * Math.sin(inicialAngle + t * (finalAngle - inicialAngle))
+            points.push(new THREE.Vector3(x, y, 0))
+        }
+
+        let curve = new THREE.CatmullRomCurve3( points )
+        let sampledPoints = curve.getPoints( this.numberOfSamples );
+        this.curveGeometry = new THREE.BufferGeometry().setFromPoints( sampledPoints )
+        this.lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } )
+        this.lineObj = new THREE.Line( this.curveGeometry, this.lineMaterial )
+        this.lineObj.position.set(position.x,position.y,position.z)
+        this.app.scene.add( this.lineObj );
+        return this.lineObj
+
+    }
+
+    drawQuarterCircunference(position, radius, numberOfSamples,reverse) {
+        let points = []
+        const inicialAngle = 0
+        const finalAngle = Math.PI / 2
+
+        for (let i = 0; i <= numberOfSamples; i++) {
+            const t = i / this.numberOfSamples
+            if (reverse === true) {
+                const x = radius * Math.cos(inicialAngle + t * (finalAngle - inicialAngle))
+                const y = radius * Math.sin(inicialAngle + t * (finalAngle - inicialAngle))
+                points.push(new THREE.Vector3(x, y, 0))
+            }
+            else {
+                const x = radius * Math.cos(finalAngle - t * (inicialAngle - finalAngle))
+                const y = radius * Math.sin(finalAngle - t * (inicialAngle - finalAngle))
+                points.push(new THREE.Vector3(x, y, 0))
+            }
+        }
+
+        let curve = new THREE.CatmullRomCurve3( points )
+        let sampledPoints = curve.getPoints( this.numberOfSamples );
+        this.curveGeometry = new THREE.BufferGeometry().setFromPoints( sampledPoints )
+        this.lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } )
+        this.lineObj = new THREE.Line( this.curveGeometry, this.lineMaterial )
+        this.lineObj.position.set(position.x,position.y,position.z)
+        this.app.scene.add( this.lineObj );
+        return this.lineObj
+    }
+
+    recompute() {
+        /*
+        if (this.quadraticBezierCurve !== null)
+            this.app.scene.remove(this.quadraticBezierCurve)
+        this.initQuadraticBezierCurve()
+        */
+
+        this.drawCar()
+
+        this.drawSpiral()
     }
 
     addSpotlights() {
@@ -270,8 +441,7 @@ class MyContents  {
 
     addWall(x,y,z,rx,ry,rz) {
         let wall = new THREE.PlaneGeometry( 10, 5 );
-        let wallMaterial = new THREE.MeshStandardMaterial({ roughness:0.1,color: "#ffffff", specular: "#000000",shininess: 150, emissive: "#000000", side: THREE.DoubleSide});
-        this.wallMesh = new THREE.Mesh( wall, wallMaterial );
+        this.wallMesh = new THREE.Mesh( wall, this.wallPlaneMaterial );
         this.wallMesh.position.x = x;
         this.wallMesh.position.y = y;
         this.wallMesh.position.z = z;
@@ -323,6 +493,8 @@ class MyContents  {
     }
 
     addBoards() {
+
+
         let planeSizeU = 10;
         let planeSizeV = 10;
         let planeUVRate = planeSizeV / planeSizeU;
@@ -370,6 +542,67 @@ class MyContents  {
         this.board_viewMesh.position.z = this.wallMeshes[2].position.z + 0.01;
         this.app.scene.add( this.board_viewMesh );
 
+    }
+
+    drawCar() {
+
+        this.board = new THREE.BoxGeometry( 6,3,0.1);
+        this.boardMesh = new THREE.Mesh( this.board, this.boardMaterial );
+        this.boardMesh.position.y = 2.5;
+        this.boardMesh.position.x = 0;
+        this.boardMesh.position.z = 0;
+        this.app.scene.add( this.boardMesh );
+        
+        //this.car = new THREE.Object3D()
+        
+        this.carSize = 5
+        const rearWheel= this.drawSemiCircunference(new THREE.Vector3(-3/this.carSize,-this.boardMesh.geometry.parameters.height/4,this.boardMesh.position.z + 0.1), 3/this.carSize, 100)
+        const frontWheel = this.drawSemiCircunference(new THREE.Vector3(7/this.carSize,-this.boardMesh.geometry.parameters.height/4,this.boardMesh.position.z + 0.1), 3/this.carSize, 100)
+        const body1= this.drawQuarterCircunference(new THREE.Vector3(2/this.carSize,-this.boardMesh.geometry.parameters.height/4,this.boardMesh.position.z + 0.1), 8/this.carSize, 100,false)
+        const body2= this.drawQuarterCircunference(new THREE.Vector3(2/this.carSize,4/this.carSize -this.boardMesh.geometry.parameters.height/4,this.boardMesh.position.z + 0.1), 4/this.carSize, 100,true)
+        const body3 = this.drawQuarterCircunference(new THREE.Vector3(6/this.carSize,-this.boardMesh.geometry.parameters.height/4,this.boardMesh.position.z + 0.1), 4/this.carSize, 100,true)
+        
+        this.boardMesh.add(rearWheel)
+        this.boardMesh.add(frontWheel)
+        this.boardMesh.add(body1)
+        this.boardMesh.add(body2)
+        this.boardMesh.add(body3)
+        
+        this.boardMesh.rotation.y = -Math.PI / 2;
+        this.boardMesh.position.x = 4.9
+        this.app.scene.add(this.boardMesh)
+
+    }
+
+    drawSpiral() {
+        const segments = 100
+        const radius = 0.1
+        const numberOfLoops = 3
+        const height = 0.1
+        let points = []
+
+        var angleStep = 2 * Math.PI / segments; // 2PI = 360º, logo vou dividir o circulo em 100 partes e desenha-las
+        var heightStep = height / segments; // 0.1 = altura da mola , logo vou dividir a mola em 100 partes e desenha-las
+
+        /*
+            x ->  cos(t) * raio
+            y ->  t * h
+            z ->  sin(t) * raio
+        */
+        for (let i = 0; i < numberOfLoops * segments; i++) {
+            const x = Math.cos(i * angleStep) * radius
+            const y = i * heightStep
+            const z = Math.sin(i * angleStep) * radius
+            points.push(new THREE.Vector3(x, y, z))
+        }
+
+        const curve = new THREE.CatmullRomCurve3(points)
+        const geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(segments))
+        const material = new THREE.LineBasicMaterial({ color: 0xdcdcdc })
+        const line = new THREE.Line(geometry, material)
+        line.position.set(-0.75,this.tampoMesh.position.y + height ,-0.75)
+        //line.rotation.x = Math.PI / 2
+        this.app.scene.add(line)
 
     }
 
@@ -436,6 +669,22 @@ class MyContents  {
      */
     update() {
         
+    }
+
+    drawHull(position, points) {
+
+       
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+        let line = new THREE.Line( geometry, this.hullMaterial );
+
+        // set initial position
+
+        line.position.set(position.x,position.y,position.z)
+
+        this.app.scene.add( line );
+
     }
 
 }
